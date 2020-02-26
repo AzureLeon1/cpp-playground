8246a87f28e426a92e5158ab7e011fe82e081960
Leon <leonwangchn@163.com>
2020-02-26 20:03:35 +0800

    c++11 and c++14

--- helloworld.cpp
+++ helloworld.cpp
@@ -7,17 +7,28 @@ using namespace std;
 int main()
 {
 
-    // valid in C++14, invalid in C++11 (列表初始化和foreach)
-    // vector<string> msg {"Hello", "C++", "World", "from", "VS Code!"};
+
+    // valid in C++11 (initializer list)
+    vector<string> msg = {"Hello", "C++", "World", "from", "VS Code!"};
+
+    // if initializer list is not supported
+    // string msg_array[5] = {"Hello", "C++", "World", "from", "VS Code!"};
+    // vector<string> msg(msg_array, msg_array+5);
+    
+
+    // valid in C++14, invalid in C++11 (foreach)
     // for (const string& word : msg)
     // {
     //     cout << word << " ";
     // }
     // cout << endl;
 
-    // valid in C++11
-    string msg_array[5] = {"Hello", "C++", "World", "from", "VS Code!"};
-    vector<string> msg(msg_array, msg_array+5);
+    // if foreach is not supoorted
+    for (int i = 0; i < msg.size(); i++) {
+        cout << msg[i] << " ";
+    }
+    cout << endl;
+
     vector<string>::iterator it;
     for (it = msg.begin(); it < msg.end(); it++)
     {
