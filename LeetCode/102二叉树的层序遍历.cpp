7a17b6141c8e6b8a0923a5b2bd202f1bf0c2a44e
Leon <leonwangchn@163.com>
2020-07-03 10:44:22 +0800

    problem 102

---
+++ LeetCode/102二叉树的层序遍历.cpp
@@ -0,0 +1,48 @@
+#include<queue>
+#include<vector>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+
+        vector<vector<int> > res;
+
+        if (root==nullptr) {
+            return res;
+        }
+
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()) {
+            vector<int> cur;
+            int row_num = q.size();
+            for (int i = 0; i < row_num; ++i) {
+                TreeNode* temp = q.front();
+                q.pop();
+                cur.push_back(temp->val);
+                if(temp->left) q.push(temp->left);
+                if(temp->right) q.push(temp->right);
+            }
+            res.push_back(cur);
+        }
+        return res;
+    }
+};
